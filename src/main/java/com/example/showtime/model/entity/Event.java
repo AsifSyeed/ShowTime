419366f754ac4044f989622c1ac970a7d0d8d69e
Asif Syeed <asifsyeed666@gmail.com>
2023-05-15 22:36:35 +0600

    id generated sequencially

--- src/main/java/com/example/showtime/model/entity/Event.java
+++ src/main/java/com/example/showtime/model/entity/Event.java
@@ -1,9 +1,6 @@
 package com.example.showtime.model.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,7 +12,8 @@ import java.util.Date;
 public class Event {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "event_sequence")
+    @SequenceGenerator(name = "event_sequence", sequenceName = "event_sequence", allocationSize = 1)
     private Long id;
 
     @Column(name = "EVENT_NAME")
