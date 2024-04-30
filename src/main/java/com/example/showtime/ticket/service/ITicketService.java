43af88d39e2805e36525b17b8e36239759e91ada
AsifSyeed <asifsyeed666@gmail.com>
2024-04-30 17:36:03 +0600

    Optimized imports

--- src/main/java/com/example/showtime/ticket/service/ITicketService.java
+++ src/main/java/com/example/showtime/ticket/service/ITicketService.java
@@ -1,6 +1,5 @@
 package com.example.showtime.ticket.service;
 
-import com.example.showtime.event.model.entity.Event;
 import com.example.showtime.ticket.model.entity.Ticket;
 import com.example.showtime.ticket.model.request.BuyTicketRequest;
 import com.example.showtime.ticket.model.response.BuyTicketResponse;
