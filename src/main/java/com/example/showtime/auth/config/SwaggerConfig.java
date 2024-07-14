90b3f1eb84fc6854a9a5cd295bf4e78fbd713cdc
AsifSyeed <asifsyeed666@gmail.com>
2024-07-14 11:45:16 +0600

    Swagger set for only dev profile

--- src/main/java/com/example/showtime/auth/config/SwaggerConfig.java
+++ src/main/java/com/example/showtime/auth/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.example.showtime.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -11,6 +12,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 
 
 @Configuration
+@Profile("dev") // Only load this configuration when the "dev" profile is active
 public class SwaggerConfig {
     @Bean
     public Docket api() {
